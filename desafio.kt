b1a49af67d0b69360dd4467ce99006355bc4b56b
Natan Santos <natantech.sa@gmail.com>
2023-10-09 16:16:43 -0300

    test: classes and function tests added on main

--- desafio.kt
+++ desafio.kt
@@ -35,6 +35,36 @@ data class Formacao(val nome: String, val areaTI: AreaTI, val nivel: Nivel, val 
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    val Usuario1 = Usuario(1, "Natan Santos", 20)
+    val Usuario2 = Usuario(2, "Lucas Fernandes", 15) 
+	val Usuario3 = Usuario(3, "Luiza Souza", 18)
+    val Usuario4 = Usuario(4, "Ester Heloisa", 19)
+                           
+    val Prof1 = Professor("Jorge Ramiro", AreaTI.BACKEND)
+    
+    val Aula1 = ConteudoEducacional("Introdução", Prof1, 30)
+    val Aula2 = ConteudoEducacional("Orientação a Objetos", Prof1, 60)
+    val AulasJavaOO = mutableListOf(Aula1, Aula2)
+    
+    val JavaOO = Formacao(
+        "Java Orientado a Objetos",
+        AreaTI.BACKEND,
+        Nivel.BASICO,
+        vagas = 2,
+        AulasJavaOO
+    )
+    
+    JavaOO.matricular(Usuario1)
+    JavaOO.matricular(Usuario2)
+    JavaOO.matricular(Usuario3)
+    JavaOO.matricular(Usuario4)
+    
+    println()
+ 	println("""|Formação ${JavaOO.nome}
+    		   |------------------------
+               |Área de estudo: ${JavaOO.areaTI}
+               |Nível: ${JavaOO.nivel} 
+               |Duração do curso: ${JavaOO.duracaoHoras()} horas
+               """.trimMargin())
 }
